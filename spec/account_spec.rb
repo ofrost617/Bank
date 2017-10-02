e55c2dd6f0b6aae7399bdd1d53a34b9ae71c61af
Olivia Frost <olivia.frost617@googlemail.com>
2017-10-02 12:05:17 +0100

    second test passing, account initialized with balance of -

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,5 +6,10 @@ describe Account do
   it 'creates a new account of class Account' do
     expect(subject).to be_a(Account)
   end
+
+  it 'account balance starts at 0' do
+    account = Account.new
+    expect(account.balance).to eq(0)
+  end
   
 end
