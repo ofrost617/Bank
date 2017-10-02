71a1250ca6cb48e2be4130e29f102c9f64dcabd1
Olivia Frost <olivia.frost617@googlemail.com>
2017-10-02 12:00:19 +0100

    Account class created, first test passing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,10 @@
+require_relative '../lib/account.rb'
+
 describe Account do
   subject { described_class.new }
 
   it 'creates a new account of class Account' do
-    expect(subject).to be_a(Accout)
+    expect(subject).to be_a(Account)
   end
   
 end
