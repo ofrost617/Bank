c6a06ca655fec6199e39626ba4fdb45f1993f22b
Olivia Frost <olivia.frost617@googlemail.com>
2017-10-03 15:26:42 +0100

    bank deposit test passing

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,10 +3,14 @@ require_relative './account'
 # account.rb
 class Bank
 
-def withdraw(account, amount)
-  account.debit(amount)
-  "Thank you for your withdrawl, your current balance is now £#{account.balance}"
-end
+  def withdraw(account, amount)
+    account.debit(amount)
+    "Thank you for your withdrawl, your current balance is now £#{account.balance}"
+  end
 
+  def deposit(account, amount)
+    account.credit(amount)
+    "Thank you for your deposit, your current balance is now £#{account.balance}"
+  end
 
 end
