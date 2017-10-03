9cc4d47b013bb4487f4f843fc85883b2fabb3f65
Olivia Frost <olivia.frost617@googlemail.com>
2017-10-03 16:30:59 +0100

    statement.rb refactored

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,12 +2,23 @@ require_relative './account.rb'
 
 # statement.rb
 class Statement
-  def statement(account)
-    puts 'date || credit || debit || balance'
-    logs = ''
-    account.transaction_details.each do |log|
-      logs += "#{log[0]} || #{log[1]} || #{log[2]} || #{log[3]}\n"
-    end
-    puts logs
+
+  attr_reader :header, :logs
+
+  def initialize
+    @header = 'date || credit || debit || balance'
   end
+  
+  def create_statement(account)
+    @logs = ''
+    account.transaction_details.each do |log|
+      @logs += "#{log[0]} || #{log[1]} || #{log[2]} || #{log[3]}\n"
+    end
+  end
+
+  def print_statement
+    puts "#{@header}\n#{@logs}"
+  end
+  
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,13 +4,16 @@ require_relative '../lib/account.rb'
 describe Statement do
   subject { described_class.new }
 
+  let(:account) { double :account }
+
   it 'shows statement' do
     account = Account.new
     account.credit(5)
     time = Time.now.strftime('%m/%d/%Y')
     header = "date || credit || debit || balance\n"
     first_log = "#{time} || 5.00 ||  || 5.00\n"
-    expect { subject.statement(account) }.to output("#{header}#{first_log}").to_stdout
+    subject.create_statement(account)
+    expect { subject.print_statement }.to output("#{header}#{first_log}").to_stdout
   end
 
   it 'can access transaction_details' do
