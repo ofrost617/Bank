86b5ccfd6a942e41ddbcdb1df25d7fac9293bc19
Olivia Frost <olivia.frost617@googlemail.com>
2017-10-05 11:59:42 +0100

    transaction class working and formatted, need to remove date method from tests

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,16 +1,34 @@
 class Transaction
   
+attr_reader :date
+
   def add(type, amount, balance)
-    [type, amount, balance]
+    if type == :credit
+      format_credit(type, amount, balance)
+    else
+      format_debit(type, amount, balance)
+    end
+    
   end
 
   private
 
-  def format_transaction
-  
+    def date
+    return Time.now.strftime('%d/%m/%Y')
+  end
+
+  def two_d_p(amount)
+    '%.2f' % amount
+  end
+
+  def format_credit(type, amount, balance)
+    "#{date} || #{two_d_p(amount)} || || #{two_d_p(balance)}"
+  end
+
+  def format_debit(type, amount, balance)
+    "#{date} || || #{two_d_p(amount)} || #{two_d_p(balance)}"
+  end
+
 end
-
-
-
 
 
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -6,14 +6,19 @@ require_relative '../lib/transactions.rb'
 describe Transaction do
   subject { described_class.new }
 
-  it 'adds transactions to an array' do
-    subject.add(:credit, 5, 5)
-    expect(subject.logs[0]).to eq([:credit, 5, 5])
-  end
-  
-  it 'formats a transaction' do
-    
-  end
-  
 
+  def date
+    return Time.now.strftime('%d/%m/%Y')
+  end
+
+  it 'formats a credit transaction' do
+    transaction_line = "#{date} || 5.00 || || 5.00"
+    expect(subject.add(:credit, 5, 5)).to eq transaction_line
+  end
+
+    it 'formats a debit transaction' do
+    transaction_line = "#{date} || || 5.00 || 0.00"
+    expect(subject.add(:debit, 5, 0)).to eq transaction_line
+  end
+  
 end
